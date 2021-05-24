b1ca259a4536f9e06b8526ea1e2f596085de7542
SuperPox <calebkohl@gmail.com>
2021-05-24 14:32:02 -0600

    commit tree ec5aa2952b1d125c7e28819200bc7ff091a7e6da

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,34 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action) {
+    switch(action.type) {       
+        case "ADD_FRIEND":
+            return {...state, friends: [...state.friends, action.friend] }
+
+        case "REMOVE_FRIEND":
+            const index = state.friends.findIndex(friend => friend.id == action.id);
+            return (
+              {...state,
+                  friends: [
+                    ...state.friends.slice(0, index),
+                    ...state.friends.slice(index + 1)
+                  ]
+              }
+            )
+           
+        default:
+            return state;
+    }   
 }
+
+
+// let newfriends = {...friend, name: "Joe", hometown: "Boston", id: "101"}
+
+// export function managePresents(state, action){
+//     switch(action.type) {
+//         case "INCREASE":
+//             return {numberOfPresents: state.numberOfPresents + 1}
+//         case "DECREASE":
+//             return {numberOfPresents: state.numberOfPresents - 1}
+//         default:
+//             return state;
+//     }
+// }
